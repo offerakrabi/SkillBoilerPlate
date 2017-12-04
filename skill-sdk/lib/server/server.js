2305b1d30af371ff9f0d67478b519e3dbbd41d7a
Offer Akrabi <offer.akrabi@il.ibm.com>
2017-12-04 10:38:00 +0200

    added sdk to package.jsom

--- skill-sdk/lib/server/server.js
+++ skill-sdk/lib/server/server.js
@@ -88,6 +88,7 @@ SwaggerExpress.create(config, function (err, swaggerExpress) {
 
 });
 let getKeys = function() {
+    console.log(__dirname);
     return fs.readFileSync(path.join(__dirname, '../../../res/assets/keys.txt'), 'utf8', function(err, contents) {
         if(err){
             logger.info('could not read keys file, error: ' + err);
